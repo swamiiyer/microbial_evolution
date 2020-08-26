6ca3b8b1797fa8494c8ae2f3160363ded7351e35
swamiiyer <swami.iyer@gmail.com>
2020-08-26 06:55:20 -0400

    distribution plots

--- visualizations.py
+++ visualizations.py
@@ -1,9 +1,8 @@
-import dill, gzip, sys
-import matplotlib.pyplot as plt
+import dill, gzip, numpy, pylab, sys
 
 
-def main():
-    fname = sys.argv[1]
+def main(args):
+    fname = args[1]
 
     fh = gzip.open(fname, "rb")
     params = dill.load(fh)
@@ -14,35 +13,74 @@ def main():
     H = dill.load(fh)
     V = dill.load(fh)
     I = dill.load(fh)
+    HOST_GTYPE = dill.load(fh)
+    HOST_MASS = dill.load(fh)
+    VIRUS_GTYPE = dill.load(fh)
     fh.close()
 
-    figure = plt.figure(figsize=(8, 6), dpi=500)
+    figure = pylab.figure(figsize=(8, 6), dpi=500)
     size = 6
-    plt.rcParams["axes.titlesize"] = size
-    plt.rcParams["axes.labelsize"] = size
-    plt.rcParams["xtick.labelsize"] = size
-    plt.rcParams["ytick.labelsize"] = size
-    plt.rcParams["legend.fontsize"] = size
+    pylab.rcParams["axes.titlesize"] = size
+    pylab.rcParams["axes.labelsize"] = size
+    pylab.rcParams["xtick.labelsize"] = size
+    pylab.rcParams["ytick.labelsize"] = size
+    pylab.rcParams["legend.fontsize"] = size
 
     axes = figure.add_subplot(4, 1, 1)
     axes.set_ylabel("DIP")
-    axes.plot(T, DIP, "k-", alpha = 0.6)
+    axes.plot(T, DIP, "k-", alpha=0.6)
 
     axes = figure.add_subplot(4, 1, 2)
     axes.set_ylabel("# of hosts")
-    axes.plot(T, H, "b-", alpha = 0.6)
+    axes.plot(T, H, "b-", alpha=0.6)
 
     axes = figure.add_subplot(4, 1, 3)
     axes.set_ylabel("# of viruses")
-    axes.plot(T, V, "r-", alpha = 0.6)
+    axes.plot(T, V, "r-", alpha=0.6)
 
     axes = figure.add_subplot(4, 1, 4)
     axes.set_xlabel("Time (h)")
     axes.set_ylabel("# of infections")
-    axes.plot(T, I, "g-", alpha = 0.6)
+    axes.plot(T, I, "g-", alpha=0.6)
 
-    plt.savefig("%s" % (fname.replace("pkl", "pdf")), format="pdf", bbox_inches="tight")
+    pylab.savefig("figure1.pdf", format="pdf", bbox_inches="tight")
+
+    pylab.figure(figsize=(8, 6), dpi=500)
+    img = pylab.imshow(HOST_GTYPE, cmap=pylab.cm.Reds, origin='lower')
+    pylab.axis("tight")
+    img.axes.set_xlabel("Time (h)")
+    img.axes.set_ylabel("Host genotype distribution")
+    bins = HOST_GTYPE.shape[0]
+    ticks = numpy.arange(0, bins, 10)
+    pylab.yticks(ticks, ticks / bins)
+    cb = pylab.colorbar(img)
+    cb.set_label("# of hosts")
+    pylab.savefig("figure2.pdf", format="pdf", bbox_inches="tight")
+
+    pylab.figure(figsize=(8, 6), dpi=500)
+    img = pylab.imshow(HOST_MASS, cmap=pylab.cm.Reds, origin='lower')
+    pylab.axis("tight")
+    img.axes.set_xlabel("Time (h)")
+    img.axes.set_ylabel("Host mass distribution")
+    bins = HOST_MASS.shape[0]
+    ticks = numpy.arange(0, bins, 10)
+    pylab.yticks(ticks, ticks / bins)
+    cb = pylab.colorbar(img)
+    cb.set_label("# of hosts")
+    pylab.savefig("figure3.pdf", format="pdf", bbox_inches="tight")
+
+    pylab.figure(figsize=(8, 6), dpi=500)
+    img = pylab.imshow(VIRUS_GTYPE, cmap=pylab.cm.Reds, origin='lower')
+    pylab.axis("tight")
+    img.axes.set_xlabel("Time (h)")
+    img.axes.set_ylabel("Virus genotype distribution")
+    bins = VIRUS_GTYPE.shape[0]
+    ticks = numpy.arange(0, bins, 10)
+    pylab.yticks(ticks, ticks / bins)
+    cb = pylab.colorbar(img)
+    cb.set_label("# of viruses")
+    pylab.savefig("figure4.pdf", format="pdf", bbox_inches="tight")
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
